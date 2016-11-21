db7cd69ca31bfde38c6d2def0edb70626bb14aa4
xumiao-1 <xum.engr@gmail.com>
2016-11-20 17:47:12 -0800

    configuration changes

--- TPMS_Sensor/.ino.cpp
+++ TPMS_Sensor/.ino.cpp
@@ -1,7 +1,7 @@
 //This is a automatic generated file
 //Please do not modify this file
 //If you touch this file your change will be overwritten during the next build
-//This file has been generated on 2016-10-27 20:43:55
+//This file has been generated on 2016-11-10 15:38:03
 
 #include "Arduino.h"
 #include "TPMS_Sensor.h"
